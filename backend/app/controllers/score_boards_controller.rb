c7be9126a206f026f8ff8543f8f3f38a22030b56
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-07-23 11:51:02 -0400

    add show to ScoreBoardsController

--- backend/app/controllers/score_boards_controller.rb
+++ backend/app/controllers/score_boards_controller.rb
@@ -3,4 +3,9 @@ class ScoreBoardsController < ApplicationController
     score_boards = ScoreBoard.all
     render json: ScoreBoardSerializer.new(score_boards)
   end
+
+  def show
+    score_board = ScoreBoard.find(params[:id])
+    render json: ScoreBoardSerializer.new(score_board)
+  end
 end
