29379eae8c5871a2d664b95968c3082796c14916
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-07-28 10:09:57 -0400

    Add leaderboards

--- backend/app/controllers/high_scores_controller.rb
+++ backend/app/controllers/high_scores_controller.rb
@@ -1,6 +1,6 @@
 class HighScoresController < ApplicationController
   def index
-    high_score = HighScore.all
-    render json: HighScoreSerializer.new(high_score)
+    high_scores = HighScore.where("score_board_id = ?", params[:score_board_id])
+    render json: HighScoreSerializer.new(high_scores)
   end
 end
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -257,22 +257,62 @@ class LeaderBoard {
 
       let card = document.createElement("div")
       card.classList = "leaderboard-card"
-      card.innerHTML = `
-        <h1>${leaderBoard.attributes.difficulty}</h1>
-      `
+      card.id = `${leaderBoard.attributes.difficulty.toLowerCase()}-leaderboard`
+      card.innerHTML = `<h6>${leaderBoard.attributes.difficulty}<h6>`
 
       document.getElementById("leaderboard-container").appendChild(card)
     }
   }
 
   static fetchHighScores(leaderBoard) {
+    let highScores
+
     fetch(`http://localhost:3000/score_boards/${leaderBoard.id}/high_scores`)
       .then(response => response.json())
-      .then(scores => LeaderBoard.displayHighScores(scores.data))
+      .then(scores => LeaderBoard.displayHighScores(leaderBoard, scores.data))
   }
 
-  static displayHighScores(scores) {
+  static displayHighScores(leaderBoard, scores) {
+    const leaderBoardId = `${leaderBoard.attributes.difficulty.toLowerCase()}-leaderboard`
+    const leaderBoardElem = document.getElementById(leaderBoardId)
+    const table = document.createElement("TABLE")
 
+    const headerRow  = document.createElement("TR")
+
+    const rankHeader = document.createElement("TH")
+    rankHeader.innerHTML = "RANK"
+    headerRow.appendChild(rankHeader)
+
+    const initialsHeader = document.createElement("TH")
+    initialsHeader.innerHTML = "NAME"
+    headerRow.appendChild(initialsHeader)
+
+    const scoreHeader = document.createElement("TH")
+    scoreHeader.innerHTML = "SCORE"
+    headerRow.appendChild(scoreHeader)
+
+    table.appendChild(headerRow)
+
+    for (let i = 0; i < scores.length; i++ ) {
+
+      const row  = document.createElement("TR")
+
+      const rank = document.createElement("TD")
+      rank.innerHTML = `${i + 1}. `
+      row.appendChild(rank)
+
+      const initials = document.createElement("TD")
+      initials.innerHTML = `${scores[i].attributes.initials} `
+      row.appendChild(initials)
+
+      const score = document.createElement("TD")
+      score.innerHTML = `${scores[i].attributes.score}`
+      row.appendChild(score)
+
+      table.appendChild(row)
+    }
+
+    leaderBoardElem.appendChild(table)
   }
 }
 
